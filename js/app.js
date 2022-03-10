fadedad5fbf498c0d4160d09e356d5937df01d04
ifty03 <ashikulislamb84@gmail.com>
2022-03-10 21:42:34 +0600

    optional bug fixed

--- js/app.js
+++ js/app.js
@@ -144,6 +144,7 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
     const likedPosts = getLikedPosts();
+    document.getElementById( "liked" ).textContent='';
     likedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "liked" ).appendChild(div);
@@ -152,6 +153,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
+    document.getElementById( "reported" ).textContent='';
     console.log(reportedPosts)
     reportedPosts.forEach((post) => {
         const div = createPost(post);
